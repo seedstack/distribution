f8b1d28d1edae153bea54e24a0a07224f54923b9
Adrien LAUER <adrien.lauer@mpsa.com>
2016-03-29 12:48:51 +0200

    Fix CLI archetype demo, remove default culture conf in web archetype

--- cli-archetype/src/main/resources/archetype-resources/src/main/java/cli/SampleCommandLineHandler.java
+++ cli-archetype/src/main/resources/archetype-resources/src/main/java/cli/SampleCommandLineHandler.java
@@ -12,13 +12,13 @@ public class SampleCommandLineHandler implements CommandLineHandler {
     @CliOption(name = "f", longName = "flag")
     private Boolean flag;
 
-    @CliOption(name = "value", valueCount = 1)
+    @CliOption(name = "v", longName = "value", valueCount = 1)
     private String value;
 
-    @CliOption(name = "map", valueCount = -1, valueSeparator = '=')
+    @CliOption(name = "m", longName = "map", valueCount = -1, valueSeparator = '=')
     private Map<String, String> map;
 
-    @CliOption(name = "array", valueCount = -1, mandatory = true, defaultValues = {"1", "2", "3"})
+    @CliOption(name = "a", longName = "array", valueCount = -1, mandatory = true, defaultValues = {"1", "2", "3"})
     private String[] array;
 
     @CliArgs(mandatoryCount = 2)
